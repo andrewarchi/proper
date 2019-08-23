fba8f26f6094185546a3f6623c5e36787da298b1
Andrew Archibald <andrew@aarchibald.com>
2019-08-23 12:14:26 -0700

    Inspect primitives, arrays, and maps

--- inspect.go
+++ inspect.go
@@ -6,26 +6,66 @@ import (
 	"go/parser"
 	"go/token"
 	"os"
+	"path/filepath"
 	"reflect"
 	"strings"
 
 	"github.com/andrewarchi/proper/proptypes"
 )
 
+type testPropType struct{ val interface{} }
+
+func (t testPropType) Format(indent int) string {
+	if t.val == nil {
+		return "<<nil>>"
+	}
+	if id, ok := t.val.(*ast.Ident); ok {
+		o := id.Obj
+		if o == nil {
+			return fmt.Sprintf("<<%[1]T %q: nil Obj>>", t.val, id.Name)
+		}
+		return fmt.Sprintf("<<%[1]T %q: %v, %q, %v, %v, %v>>", t.val, id.Name, o.Kind, o.Name, o.Decl, o.Data, o.Type)
+	}
+	return fmt.Sprintf("<<%[1]T %[1]v>>", t.val)
+}
+
+var _ proptypes.PropType = testPropType{nil}
+
+func InspectDirRecursive(fset *token.FileSet, root string) (map[string][]proptypes.PropType, error) {
+	types := make(map[string][]proptypes.PropType)
+	err := filepath.Walk(root, func(path string, f os.FileInfo, err error) error {
+		if f.IsDir() {
+			return inspectDir(fset, path, types)
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return types, nil
+}
+
 func InspectDir(fset *token.FileSet, dir string) (map[string][]proptypes.PropType, error) {
+	types := make(map[string][]proptypes.PropType)
+	if err := inspectDir(fset, dir, types); err != nil {
+		return nil, err
+	}
+	return types, nil
+}
+
+func inspectDir(fset *token.FileSet, dir string, types map[string][]proptypes.PropType) error {
 	pkgs, err := parser.ParseDir(fset, dir, func(info os.FileInfo) bool {
 		return !strings.HasSuffix(info.Name(), "_test.go")
 	}, 0)
 	if err != nil {
-		return nil, err
+		return err
 	}
-	types := make(map[string][]proptypes.PropType)
 	for _, pkg := range pkgs {
 		for name, f := range pkg.Files {
 			types[name] = inspectFile(fset, f)
 		}
 	}
-	return types, nil
+	return nil
 }
 
 func inspectFile(fset *token.FileSet, f *ast.File) []proptypes.PropType {
@@ -35,14 +75,14 @@ func inspectFile(fset *token.FileSet, f *ast.File) []proptypes.PropType {
 			fmt.Println(fset.Position(typeDecl.Pos()))
 			for _, s := range typeDecl.Specs {
 				if spec, ok := s.(*ast.TypeSpec); ok {
-					fmt.Println("#", spec.Name)
-					propType := inspectExpr(spec.Type)
-					if propType != nil {
-						fmt.Println("PROPTYPE: ", propType.Format(0))
+					fmt.Printf("const %s = ", spec.Name)
+					if propType, ok := inspectExpr(spec.Type); ok {
+						fmt.Print(propType.Format(0))
 						propTypes = append(propTypes, propType)
 					} else {
-						fmt.Println("PROPTYPE: nil")
+						fmt.Print("null")
 					}
+					fmt.Println(";")
 				}
 			}
 			fmt.Println()
@@ -51,40 +91,84 @@ func inspectFile(fset *token.FileSet, f *ast.File) []proptypes.PropType {
 	return propTypes
 }
 
-func inspectExpr(expr ast.Expr) proptypes.PropType {
-	fmt.Println(reflect.TypeOf(expr).Elem().Name())
+func inspectExpr(expr ast.Expr) (proptypes.PropType, bool) {
 	switch typ := expr.(type) {
 	case *ast.Ident:
+		return inspectIdent(typ)
 	case *ast.ParenExpr:
 	case *ast.SelectorExpr:
 	case *ast.StarExpr:
 		return inspectExpr(typ.X)
 	case *ast.ArrayType:
-		return proptypes.ArrayOf(inspectExpr(typ.Elt))
+		return inspectArray(typ.Elt)
+	case *ast.InterfaceType:
+		// Interfaces are dynamic, so it would be difficult to detect all the types
+		// that satisfy the interface in the codebase. Ideally, this would be a
+		// OneOfType of all types satisfying the interface rather than Object.
+		return proptypes.Object, true
 	case *ast.MapType:
-		return inspectMap(typ)
+		if t, ok := inspectExpr(typ.Value); ok {
+			return proptypes.ObjectOf(t), true
+		}
+		return proptypes.Object, true
 	case *ast.StructType:
 		return inspectStruct(typ)
-	case *ast.ChanType, *ast.FuncType, *ast.InterfaceType:
-		return nil // cannot be marshalled to json
+	case *ast.ChanType, *ast.FuncType:
+		// Channels and funcs cannot encoded in json. Attempting to encode such a
+		// value causes jsonMarshal to return a json.UnsupportedTypeError.
+		return nil, false
 	default:
 		fmt.Println("Other")
+		return testPropType{fmt.Sprint("UNMATCHED", reflect.TypeOf(expr).Elem().Name())}, true
 	}
-	return nil
+	return testPropType{expr}, true
 }
 
-func inspectMap(m *ast.MapType) proptypes.PropType {
-	return proptypes.Shape(nil) // only if key is string
+func inspectIdent(id *ast.Ident) (proptypes.PropType, bool) {
+	if id.Obj == nil {
+		// TODO(aa) Is there a better way of detecting Go primitives?
+		// List of primitives taken from https://golang.org/ref/spec#Types.
+		switch id.Name {
+		case "bool":
+			return proptypes.Bool, true
+		case "uint8", "uint16", "uint32", "uint64",
+			"int8", "int16", "int32", "int64",
+			"float32", "float64",
+			"byte", "rune",
+			"uint", "int", "uintptr":
+			return proptypes.Number, true
+		case "complex64", "complex128":
+			return nil, false
+		case "string":
+			return proptypes.String, true
+		}
+	}
+	return testPropType{id}, true
 }
 
-func inspectStruct(typ *ast.StructType) proptypes.PropType {
+// inspectArray returns a prop type representing an array. As a special case,
+// []byte is encoded to json as a string. For any invalid element types, an
+// untyped array is returned.
+func inspectArray(elem ast.Expr) (proptypes.PropType, bool) {
+	if e, ok := elem.(*ast.Ident); ok && e.Name == "byte" && e.Obj == nil {
+		return proptypes.String, true
+	}
+	if t, ok := inspectExpr(elem); ok {
+		return proptypes.ArrayOf(t), true
+	}
+	return proptypes.Array, true
+}
+
+func inspectStruct(typ *ast.StructType) (proptypes.PropType, bool) {
 	var shape proptypes.ShapeMap
 	if typ.Fields != nil {
 		for _, field := range typ.Fields.List {
-			propType := inspectExpr(field.Type)
+			propType, ok := inspectExpr(field.Type)
+			if !ok {
+				continue
+			}
 			for _, name := range field.Names {
-				fmt.Print(field)
-				if !name.IsExported() { // undetectable in json marshall
+				if !name.IsExported() { // undetectable in json marshal
 					continue
 				}
 				fieldName := name.Name
@@ -99,10 +183,10 @@ func inspectStruct(typ *ast.StructType) proptypes.PropType {
 					}
 					omitEmpty = options.Contains("omitempty")
 				}
+				_ = omitEmpty
 				shape = append(shape, proptypes.ShapeEntry{Name: fieldName, Type: propType})
-				fmt.Println(fieldName, omitEmpty)
 			}
 		}
 	}
-	return proptypes.Shape(shape)
+	return proptypes.Shape(shape), true
 }
