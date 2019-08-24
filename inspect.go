ad14de6178fd90b3acad132dbd80c2c425a110d6
Andrew Archibald <andrew@aarchibald.com>
2019-08-23 19:04:36 -0700

    Partial selector implementation

--- inspect.go
+++ inspect.go
@@ -15,37 +15,46 @@ import (
 
 // PropTypeDecl is a declaration of a prop type in JavaScript.
 type PropTypeDecl struct {
-	name *ast.Ident
-	pos  token.Pos
-	typ  proptypes.PropType
+	Name *ast.Ident
+	Pos  token.Pos
+	Typ  proptypes.PropType
 }
 
 // Format formats a prop type declaration as a JavaScript const declaration.
 func (d *PropTypeDecl) Format(fset *token.FileSet) string {
 	t := "null"
-	if d.typ != nil {
-		t = d.typ.Format(0)
+	if d.Typ != nil {
+		t = d.Typ.Format(0)
 	}
-	return fmt.Sprintf("// %v\nconst %s = %s;", fset.Position(d.pos), d.name, t)
+	return fmt.Sprintf("// %v\nconst %s = %s;", fset.Position(d.Pos), d.Name, t)
+}
+
+var _ proptypes.PropType = (*selectorPropType)(nil)
+var _ proptypes.PropType = (*testPropType)(nil)
+
+type selectorPropType struct {
+	sel *ast.SelectorExpr
+}
+
+func (s *selectorPropType) Format(indent int) string {
+	panic("unimplemented")
 }
 
 type testPropType struct{ val interface{} }
 
-func (t testPropType) Format(indent int) string {
+func (t *testPropType) Format(indent int) string {
 	if t.val == nil {
 		return "<<nil>>"
 	}
 	if id, ok := t.val.(*ast.Ident); ok {
 		o := id.Obj
 		if o == nil {
-			return fmt.Sprintf("<<%[1]T %q: nil Obj>>", t.val, id.Name)
+			return fmt.Sprintf("<<%T %q: nil Obj>>", t.val, id.Name)
 		}
-		return fmt.Sprintf("<<%[1]T %q: %v, %q, %v, %v, %v>>", t.val, id.Name, o.Kind, o.Name, o.Decl, o.Data, o.Type)
+		return fmt.Sprintf("<<%T %q: %v, %q, %v, %v, %v>>", t.val, id.Name, o.Kind, o.Name, o.Decl, o.Data, o.Type)
 	}
 	return fmt.Sprintf("<<%[1]T %[1]v>>", t.val)
 }
-
-var _ proptypes.PropType = testPropType{nil}
 
 func InspectDirRecursive(root string, fset *token.FileSet, types map[string][]*PropTypeDecl) error {
 	return filepath.Walk(root, func(path string, f os.FileInfo, err error) error {
@@ -92,6 +101,7 @@ func inspectExpr(expr ast.Expr) (proptypes.PropType, bool) {
 		return inspectIdent(typ)
 	case *ast.ParenExpr:
 	case *ast.SelectorExpr:
+		return inspectSelector(typ)
 	case *ast.StarExpr:
 		return inspectExpr(typ.X)
 	case *ast.ArrayType:
@@ -99,8 +109,8 @@ func inspectExpr(expr ast.Expr) (proptypes.PropType, bool) {
 	case *ast.InterfaceType:
 		// Interfaces are dynamic, so it would be difficult to detect all the types
 		// that satisfy the interface in the codebase. Ideally, this would be a
-		// OneOfType of all types satisfying the interface rather than Object.
-		return proptypes.Object, true
+		// OneOfType specifying all types satisfying the interface rather than Any.
+		return proptypes.Any, true
 	case *ast.MapType:
 		if t, ok := inspectExpr(typ.Value); ok {
 			return proptypes.ObjectOf(t), true
@@ -114,9 +124,37 @@ func inspectExpr(expr ast.Expr) (proptypes.PropType, bool) {
 		return nil, false
 	default:
 		fmt.Println("Other")
-		return testPropType{fmt.Sprint("UNMATCHED", reflect.TypeOf(expr).Elem().Name())}, true
+		return &testPropType{fmt.Sprint("UNMATCHED", reflect.TypeOf(expr).Elem().Name())}, true
 	}
-	return testPropType{expr}, true
+	return &testPropType{expr}, true
+}
+
+func inspectSelector(expr *ast.SelectorExpr) (proptypes.PropType, bool) {
+	if x, ok := expr.X.(*ast.Ident); ok && x != nil && x.Obj != nil {
+		fmt.Println("XOBJ", x.Obj)
+	}
+	if expr.Sel != nil && expr.Sel.Obj != nil {
+		fmt.Println("SELOBJ", expr.Sel.Obj)
+	}
+	fmt.Println("SELECTOR", expr)
+	if expr.X != nil && expr.Sel != nil {
+		if id, ok := expr.X.(*ast.Ident); ok && id.Obj == nil && expr.Sel.Obj == nil {
+			sel := expr.Sel.Name
+			switch id.Name {
+			case "time":
+				switch sel {
+				case "Time":
+					return proptypes.String, true
+				}
+			case "bson":
+				switch sel {
+				case "ObjectId":
+					return proptypes.String, true
+				}
+			}
+		}
+	}
+	return nil, false
 }
 
 func inspectIdent(id *ast.Ident) (proptypes.PropType, bool) {
@@ -138,7 +176,7 @@ func inspectIdent(id *ast.Ident) (proptypes.PropType, bool) {
 			return proptypes.String, true
 		}
 	}
-	return testPropType{id}, true
+	return &testPropType{id}, true
 }
 
 // inspectArray returns a prop type representing an array. As a special case,
